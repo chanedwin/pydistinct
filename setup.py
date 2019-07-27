0069a12ccaebd5d5bebc38a15c1908a7e1fbc734
edwinchan91 <chan.edwin.91@gmail.com>
2019-07-28 01:21:17 +0800

    update long description in pypi repo

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="pydistinct",
     version="0.3",
     author="Edwin Chan",
     author_email="edwinchan@u.nus.edu",
     description="Package for estimating distinct values in a population",
-    long_description=long_description,
+    long_description=""" This package provides statistical estimators to predict a population's total number of distinct values from a sample sequence - given a sample sequence of n values with only d distinct values, predict the total number of distinct values D that exists in the population N.
+
+    Sample use cases :
+    
+    estimating the number of unique insects in a population from a field sample
+    estimating the number of unique words in a document given a sentence or a paragraph
+    estimating the number of unique items in a database from a few sample rows
+    Please send all bugs reports/issues/queries to chanedwin91@gmail.com for fastest response!
+    
+    See https://github.com/chanedwin/pydistinct for more information 
+    """,
     long_description_content_type="text/markdown",
     url="https://github.com/chanedwin/pydistinct/",
-    download_url='https://github.com/chanedwin/pydistinct/archive/v0.3.tar.gz', 
-    keywords=['distinct', 'value', 'estimators', 'sample', 'sequences'], 
-    install_requires=[ 
+    download_url='https://github.com/chanedwin/pydistinct/archive/v0.3.tar.gz',
+    keywords=['distinct', 'value', 'estimators', 'sample', 'sequences'],
+    install_requires=[
         'scipy',
         'statsmodels',
         'xgboost',
